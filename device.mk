c11fbca08ebb09695cc0b4f019a824d62921be8f
Janson Kang <temasek71@gmail.com>
2015-03-23 07:29:58 +0800

    hlte: Enable OTA update

--- device.mk
+++ device.mk
@@ -14,6 +14,11 @@
 # limitations under the License.
 #
 
+PRODUCT_PROPERTY_OVERRIDES := \
+    ro.ota.romname=temasek-hlte \
+    ro.ota.version=$(shell date +%F | sed s@-@@g) \
+    ro.ota.manifest=http://temasek.rajasthanautoworks.in/ota/hlte.xml
+
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
 # Get non-open-source specific aspects
